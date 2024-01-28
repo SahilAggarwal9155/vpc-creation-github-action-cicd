674892150379ff54ff1a38b4ca057b7577a6b7cb
SahilAggarwal9155 <111456510+SahilAggarwal9155@users.noreply.github.com>
2024-01-29 01:28:39 +0530

    Update providers.tf of config file

--- terraform-vpc/providers.tf
+++ terraform-vpc/providers.tf
@@ -11,7 +11,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "new-terraform-vpc"
 
 }
